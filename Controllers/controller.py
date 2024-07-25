1ca2c3f251d2a3a5a293ef081e6524e6af8febf6
Clément | Igonogo <clement@igonogo.io>
2024-07-25 16:54:06 +0200

    Allowing to transform the object Tournoi in dict

--- Controllers/controller.py
+++ Controllers/controller.py
@@ -4,6 +4,9 @@ class Controller :
     def __init__(self, view) -> None:
         pass
 
+
+
+
     def create_tournoi(self):
         print("Création d'un tournoi, veuillez renseigner les informations suivantes : ")
         name = input("Nom du tournoi : ")
@@ -11,8 +14,12 @@ class Controller :
         debut_date = input("Date de début du tournoi : ")
         end_date = input("Date de fin du tournoi : ")
         nb_rounds = input("Nombre de rounds : ")
+        current_round = 1
         description = input("Description du tournoi : ")
-        tournoi = Tournoi(name, location, debut_date, end_date, nb_rounds, description)
-        print("Tournoi créé avec succès ! : " + tournoi)
+        rounds=[]
+        players = []
+        previous_matches = []   
+        tournoi = Tournoi(name, location, debut_date, end_date, nb_rounds, current_round, players, description, rounds, previous_matches)
+        print("Tournoi créé avec succès ! : " , tournoi.to_dict())
         return tournoi
 
--- Models/tournoi.py
+++ Models/tournoi.py
@@ -1,12 +1,31 @@
+
+from datetime import datetime
+
 class Tournoi : 
    
 
-    def init(self, name, location, debut_date, nb_rounds, players, description, previous_matches) : 
+    def __init__(self, name, location, debut_date, end_date, nb_rounds, current_round, players, description : str, rounds, previous_matches : list ) -> None: 
         self.name = name
         self.location = location
-        self.debut_date = debut_date
+        self.debut_date = datetime.strptime(debut_date, "%Y-%m-%d") if debut_date else datetime.now()
+        self.end_date = datetime.strptime(end_date, "%Y-%m-%d") if end_date else None
         self.nb_rounds = nb_rounds if nb_rounds else 4
+        self.current_round = current_round if current_round else 1
+        self.rounds = rounds if rounds else []
         self.players = players if players else []
-        self.description = description
-        self.rounds = []
-        self.previous_matches = previous_matches if previous_matches else []   
+        self.description = description if description else ""
+        self.previous_matches = previous_matches if previous_matches else []  
+        
+    def to_dict(self):
+        return {
+                "name": self.name,
+                "location": self.location,
+                "debut_date": self.debut_date.strftime("%Y-%m-%d") if self.debut_date else None,
+                "end_date": self.end_date.strftime("%Y-%m-%d") if self.end_date else None,
+                "nb_rounds": self.nb_rounds,
+                "current_round": self.current_round,
+                "rounds": self.rounds,
+                "players": self.players,
+                "description": self.description,
+                "previous_matches": self.previous_matches
+            }
